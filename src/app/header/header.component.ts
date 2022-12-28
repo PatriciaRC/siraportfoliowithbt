60e169d36095065d6dfb02953ed2344db3d9d508
PatriciaRC <patriciaromero2106@gmail.com>
2022-12-28 20:36:24 +0100

    navbar done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,12 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <p>
-      header works!
-    </p>
+   <nav class="nav justify-content-end mt-5">
+  <a class="nav-link mr-2" routerLink="/portfolio">Portfolio</a>
+  <a class="nav-link mr-2" routerLink="/contact">Contact</a>
+  <a class="nav-link mr-4" routerLink="/about">About me</a>
+</nav>
+     
   `,
-  styles: [
-  ]
+  styles: [`
+
+  .nav-link {
+    font-family: 'Epilogue' !important;
+    font-style: normal !important;
+    font-weight: 300 !important;
+    font-size: 20px !important;
+    line-height: 148.5% !important;
+    color: #252525 !important;
+  };
+  .mr-4 {
+    margin-right: 4em !important;
+  };
+  .mr-2 {
+    margin-right: 2em !important;
+  }
+  
+
+  `]
 })
 export class HeaderComponent implements OnInit {
 
